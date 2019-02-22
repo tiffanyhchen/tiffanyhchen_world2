0f52ee3f654782f064e32d5f0f1c10bfb63cddc2
Nawon Choi <nawonc@andrew.cmu.edu>
2019-02-21 22:02:22 -0500

    article model set up

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ApplicationRecord
+   belongs_to :category
+
+    validates_presence_of :title
+    validates_presence_of :content
+
+
+    scope :alphabetical, -> { order('title') }
+    scope :active,       -> { where(active: true) }
+    
+end
